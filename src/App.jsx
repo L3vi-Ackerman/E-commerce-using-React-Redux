e87dbd6b03befd71d0eaa681b7a4bdff6020ee70
L3vi Ackerman <l3vi3211@gmail.com>
2025-01-02 23:31:15 +0545

    updated the cart ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import Header from './Components/Header'
+import React from 'react';
+import Header from './Components/Header';
+import CardsDetails from './Components/CardsDetails';
+import Cards from './Components/Cards';
+import {Routes,Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
   return (
     <>
-    <Header />
+    <Header /> 
+    <Routes>
+      <Route path='/' element={<Cards/>} />
+      <Route path='cart' element={<CardsDetails />} />
+    </Routes>
     </>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,25 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Badge from "@mui/material/Badge";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+import { NavLink } from "react-router-dom";
 const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark" data-bs-theme="dark" style={{ height: "60px" }}>
         <Container>
-          <Navbar.Brand href="#home">Add to Cart</Navbar.Brand>
+          <NavLink to="/" className="text-decoration-none text-light mx-3">
+            Add to Cart
+          </NavLink>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <NavLink to="/" className="text-decoration-none text-light">
+              Home
+            </NavLink>
           </Nav>
-          <Badge badgeContent={4} color="primary">
-            <MailIcon color="action" />
+          <Badge
+            badgeContent={4}
+            color="primary"
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
             <i
               class="fa-solid fa-cart-shopping text-light"
               style={{ fontSize: 20, cursor: "pointer" }}
             ></i>
           </Badge>
         </Container>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <div className="card_details">
+            <i className="fas fa-close"></i>
+
+            <p>Your Cart is Empty</p>
+            <img src="./assets/cart.gif"></img>
+          </div>
+        </Menu>
       </Navbar>
     </>
   );
