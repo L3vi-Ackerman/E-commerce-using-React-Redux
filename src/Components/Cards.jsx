a5eab69f6ee1d68f407a3eb80b6a9a0065cb48e4
L3vi Ackerman <l3vi3211@gmail.com>
2025-01-03 22:09:04 +0545

    UI designing completed

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,25 +2,33 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import CardsData from "./CardsData";
+import "./style.css";
 const Cards = () => {
   const [data, setData] = useState(CardsData);
 
   return (
     <div className="container mt-3">
       <h2 className="text-center">Items </h2>
-      <div className="row">
+      <div className="row d-flex justify-content-center align-items-center">
         {data.map((element, id) => {
           return (
             <>
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src={element.imgdata} />
+              <Card
+                style={{ width: "22rem", borer: "none" }}
+                className="mx-2 mt-4 card_style"
+              >
+                <Card.Img
+                  variant="top"
+                  src={element.imgdata}
+                  style={{ height: "16rem" }}
+                  className="mt-3"
+                />
                 <Card.Body>
                   <Card.Title>{element.rname}</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Card.Text>Price: Rs. {element.price}</Card.Text>
+                  <div className="button_div d-flex justify-content-center">
+                    <Button variant="primary" className="col-lg-12">Add to Cart</Button>
+                  </div>
                 </Card.Body>
               </Card>
             </>
--- src/Components/CardsDetails.jsx
+++ src/Components/CardsDetails.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
-
+import React from "react";
+import Table from "react-bootstrap/Table";
 const CardsDetails = () => {
   return (
-    <div>CardsDetails</div>
-  )
-}
+    <>
+      <div className="container mt-2">
+        <h2 className="text-center">Item Details</h2>
+        <section className="container mt-3">
+          <div className="itemsdetails d-flex">
+            <div className="items_img">
+              <img src="/assets/Shrimp_Tempura.png" alt="" />
+            </div>
+            <div className="details">
+              <div className="details">
+                <Table>
+                  <tr>
+                    <td>
+                      <p>
+                        {" "}
+                        <strong>Restaurant</strong> : random
+                      </p>
+                      <p>
+                        {" "}
+                        <strong>Price</strong> : Rs. random
+                      </p>
+                      <p>
+                        {" "}
+                        <strong>Dishes</strong> : random
+                      </p>
+                      <p>
+                        {" "}
+                        <strong>Total</strong> :Rs. random
+                      </p>
+                      {/* <div className='mt-5 d-flex justify-content-between align-items-center' style={{width:100,cursor:"pointer",background:"#ddd",color:"#111"}}>
+                    <span style={{fontSize:24}} onClick={ele.qnty <=1 ? ()=>dlt(ele.id) : ()=>remove(ele)}>-</span>
+                    <span style={{fontSize:22}}>{ele.qnty}</span>
+                    <span style={{fontSize:24}} onClick={()=>send(ele)}>+</span>
 
-export default CardsDetails
+                    </div> */}
+                    </td>
+                    <td>
+                      <p>
+                        <strong>Rating: </strong>
+
+                        <span
+                          style={{
+                            background: "green",
+                            color: "#fff",
+                            padding: "2px 5px",
+                            borderRadius: "5px",
+                          }}
+                        >
+                          3.5 ★
+                        </span>
+                      </p>
+                      <p>
+                        <strong>Order Review: </strong>
+
+                        <span>1175+ order placed from here recently</span>
+                      </p>
+                      <p>
+                        <strong>Rating: </strong>
+
+                        <span>
+                          <i className="fas fa-trash" style={{color:'red',fontSize:20,cursor:'pointer'}}></i>
+                        </span>
+                      </p>
+                    </td>
+                  </tr>
+                </Table>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default CardsDetails;
